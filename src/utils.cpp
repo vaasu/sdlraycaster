be9162d77024d477588b813d5062f4cac1a429b6
Ibrahim Fayaz <phayax at gmail dot com>
2020-01-08 19:09:07 +0100

    add homogenization

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,8 +1,13 @@
 #include "utils.h"
+#include <glm/ext.hpp>
+#include <glm/glm.hpp>
 
 namespace sdlraycaster {
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b,
                     const uint8_t a) {
   return (a << 24) + (b << 16) + (g << 8) + r;
+}
+glm::vec4 homogenize(glm::vec4 v) {
+  return glm::vec4(v.x / v.w, v.y / v.w, v.z / v.w, 1.0f);
 }
 }  // namespace sdlraycaster
--- src/utils.h
+++ src/utils.h
@@ -5,9 +5,13 @@
 #include <stdint.h>
 #include <SFML/Graphics.hpp>
 
+#include <glm/ext.hpp>
+#include <glm/glm.hpp>
+
 namespace sdlraycaster {
 uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b,
                     const uint8_t a = 255);
+glm::vec4 homogenize(glm::vec4 v);
 struct Rgba {
   sf::Uint8 r{0};
   sf::Uint8 g{0};
